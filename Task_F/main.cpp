6c259739f39999570a0fe462c4be221f42a4e349
Mikhail Stepanov <80860612+1MikhailStepanov1@users.noreply.github.com>
2022-03-25 14:25:55 +0300

    Update main.cpp

--- Task_F/main.cpp
+++ Task_F/main.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <list>
 
 using namespace std;
 
 int main() {
-    int n = 0;
+
     string input;
     vector<string> answer;
-    while (n != 4){
-        cin >> input;
+
+	//Ввод данных
+    while (cin >> input){
         answer.push_back(input);
-        n +=1;
     }
-
-//    for (int i = 0; i < answer.size(); i++){
-//        for (int j = 0; j<answer.size(); j++){
-//            if (answer[j-1] + answer[j] < answer[j] + answer[j-1]){
-//                string temp = answer[j];
-//                answer[j] = answer[j-1];
-//                answer[j-1] = temp;
-//            }
-//        }
-//    }
-
+	
+	//Тело программы
     for (auto it = answer.begin(); it != answer.end(); it++){
         for (auto iter = answer.begin()+1; iter != answer.end(); iter++){
             if ((*iter + *(iter-1)) > (*(iter-1) + *iter)){
@@ -32,7 +25,8 @@ int main() {
             }
         }
     }
-
+	
+	//Вывод данных
     for (auto & it : answer){
         cout << it;
     }
